e1451e53b6619e844f0d81bfa6ae572817ca4cdb
macko2 <mackozx@gmail.com>
2018-10-21 15:26:23 +0200

    Dodano testy.

--- cards/src/test/java/blackjack/app/cards/AppTest.java
+++ cards/src/test/java/blackjack/app/cards/AppTest.java
@@ -3,11 +3,27 @@ package blackjack.app.cards;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.util.List;
+import java.util.ArrayList;
 
 public class AppTest extends TestCase {
  
-    public void testApp()
-    {
+    public void testApp() {
         assertTrue( true );
+    }
+    
+    public void testGetHandSize() {
+    	Blackjack b = new Blackjack();
+    	int n = 10;
+    	for(int i = 0; i < n; i++) {
+    		b.hit();
+    	}
+    	assertEquals(n + 2, b.getHandSize());
+    }
+    
+    public void testDeckValue() {
+    	List<Card> deck = Card.createDeck();
+    	int fulldeckvalue = 340;
+    	assertEquals(fulldeckvalue, Card.deckValue(deck));
     }
 }
